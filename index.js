b428dce9982b0ea84b277bf76fb64ff7c04b675e
Norman Matos <nmatos419@hotmail.com>
2018-05-27 20:26:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -98,5 +98,9 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
-  if (cardNumber)
+  if (cardNumber == null){
+    return "Sorry, we don't have a credit card on file for you."
+  }else{
+    var total = total()
+  }
 }
