98d6e218e6ac34856821351843109bfd0ed72caa
Norman Matos <nmatos419@hotmail.com>
2018-05-27 18:45:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,14 +12,15 @@ function setCart(c) {
 function addToCart(item) {
  // write your code here
  
- //Randomly assign a price between 1 and 100
+ //Randomly assign an item price between 1 and 100
  itemPrice = Math.floor((Math.random()*100) + 1)
  
+ //Create object with name of the item as a key and price as a value
  var itemObj = Object.assign({},{'itemName':item, 'itemPrice':itemPrice})
+ 
  cart.push(itemObj)
  
- return 
- 
+ return `${item} has been added to your cart.`
  
 }
 
