c0dddef7ac70d3f5a8ec1dd933aa7f9e8fb990c4
Norman Matos <nmatos419@hotmail.com>
2018-05-27 19:10:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -44,9 +44,15 @@ function viewCart() {
       
       //record itemInfo to add to cartInfo
       if (i === cart.length - 1 && cart.length === 1){
+        //if there's only one item in the array
         itemInfo = `${item} at ${price}.`
       }else if (i === cart.length -1 && cart.length !== 1){
-        itemInfo = `and ${item} at ${price}
+        //if item identified is the last item left in the cart
+        itemInfo = `and ${item} at ${price}.`
+      }else{
+        //since the item identified is certainly not last in the cartInfo
+        itemInfo = `${item} at ${price}, `
+        
       }
       
       cartInfo += itemInfo
